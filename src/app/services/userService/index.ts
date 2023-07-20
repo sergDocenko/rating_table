385e81e4f0f3c4917b522f556a685030619b68c0
docenkoserg <serg.docenko0@gmail.com>
2023-07-20 17:00:35 +0300

    fix small bug

--- src/app/services/userService/index.ts
+++ src/app/services/userService/index.ts
@@ -1,7 +1,7 @@
 import httpServise from "../HTTP";
 import { RequestParams } from "../../types";
 
-const baseURL = "http://localhost:3000/api/users";
+const baseURL = "api/users";
 
 export async function fetchUsers(params?: RequestParams) {
   const response = await httpServise.get(baseURL, params);
